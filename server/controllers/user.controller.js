6f34bc4efb143a1d425b2e28e45ff3dc74848274
Sgrahminator <126929663+Sgrahminator@users.noreply.github.com>
2024-01-04 16:27:21 -0700

    update

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,8 +1,36 @@
 const User = require('../models/user.model');
+const Submission = require('../models/submission.model');
+const Review = require('../models/review.model');
 
 exports.getUserProfile = async (req, res) => {
     try {
-        res.status(200).json({ userProfile: req.user });
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        const userSubmissions = await Submission.find({ user: userId }).populate('reviews');
+        const userReviews = await Review.find({ user: userId });
+
+        res.status(200).json({ 
+            userProfile: user, 
+            submissions: userSubmissions, 
+            reviews: userReviews 
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getOtherUserProfile = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const user = await User.findById(userId, 'username');
+        const userSubmissions = await Submission.find({ user: userId }).populate('reviews');
+        const userReviews = await Review.find({ user: userId });
+
+        res.status(200).json({ 
+            userProfile: user, 
+            submissions: userSubmissions, 
+            reviews: userReviews 
+        });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -21,18 +49,6 @@ exports.deleteUserProfile = async (req, res) => {
     try {
         await User.findByIdAndDelete(req.user._id);
         res.status(200).json({ message: 'User profile deleted' });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
-
-exports.getOtherUserProfile = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        res.status(200).json({ userProfile: user });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }

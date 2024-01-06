78a8a4f43279e12285c93717676bdd5b69f7c172
Sgrahminator <126929663+Sgrahminator@users.noreply.github.com>
2024-01-05 23:00:57 -0700

    updates

--- server/controllers/submission.controller.js
+++ server/controllers/submission.controller.js
@@ -1,9 +1,7 @@
 const Submission = require('../models/submission.model');
 const Review = require('../models/review.model');
-const User = require('../models/user.model'); 
 
 const SubmissionController = {
-    // Create a new submission
     createSubmission: async (req, res) => {
         try {
             const newSubmission = new Submission({ ...req.body, user: req.user._id });
@@ -14,50 +12,91 @@ const SubmissionController = {
         }
     },
 
-    // Get details of a specific submission
     getSubmissionById: async (req, res) => {
         try {
-            const submission = await Submission.findById(req.params.id).populate('user', 'name');
+            const submission = await Submission.findById(req.params.id)
+                                            .populate('user', 'name')
+                                            .populate('categories');
             if (!submission) {
                 return res.status(404).json({ message: 'Submission not found' });
             }
-            res.status(200).json(submission);
+            const reviews = await Review.find({ submission: req.params.id });
+            res.status(200).json({ submission, reviews });
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
     },
 
-    // Get a list of submissions with optional filters
     getSubmissions: async (req, res) => {
         try {
-            const query = {}; // Build query based on filters like category, location, etc.
-            const submissions = await Submission.find(query).populate('user', 'name');
+            let query = {};
+            let sort = {};
+            let aggregatePipeline = [];
+
+            // Filter by location
+            if (req.query.location) {
+                const location = await Location.findOne({ name: req.query.location });
+                if (location) {
+                    query['location'] = location._id;
+                }
+            }
+
+            // Base aggregation pipeline
+            aggregatePipeline.push({ $match: query });
+            aggregatePipeline.push({ $lookup: { from: 'reviews', localField: '_id', foreignField: 'submission', as: 'reviews' } });
+            aggregatePipeline.push({ $unwind: { path: '$reviews', preserveNullAndEmptyArrays: true } });
+            aggregatePipeline.push({ $group: { _id: '$_id', doc: { $first: '$$ROOT' }, averageRating: { $avg: '$reviews.rating' } } });
+            aggregatePipeline.push({ $replaceRoot: { newRoot: { $mergeObjects: ['$doc', { averageRating: '$averageRating' }] } } });
+
+            // Sorting logic
+            switch (req.query.sort) {
+                case 'highestRatings':
+                    aggregatePipeline.push({ $sort: { averageRating: -1 } });
+                    break;
+                case 'recentAdditions':
+                    sort = { createdAt: -1 };
+                    break;
+                case 'oldestSubmissions':
+                    sort = { createdAt: 1 };
+                    break;
+            }
+
+            if (req.query.sort !== 'highestRatings') {
+                aggregatePipeline.push({ $sort: sort });
+            }
+
+            const submissions = await Submission.aggregate(aggregatePipeline)
+                                                .populate('user', 'name')
+                                                .populate('categories')
+                                                .populate('location');
+
             res.status(200).json(submissions);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
     },
 
-    // Update a submission
     updateSubmission: async (req, res) => {
         try {
-            const submission = await Submission.findOne({ _id: req.params.id, user: req.user._id });
+            const submission = await Submission.findOneAndUpdate(
+                { _id: req.params.id, user: req.user._id },
+                req.body,
+                { new: true }
+            );
             if (!submission) {
                 return res.status(404).json({ message: 'Submission not found or user not authorized' });
             }
-
-            Object.assign(submission, req.body);
-            await submission.save();
             res.status(200).json(submission);
         } catch (error) {
             res.status(400).json({ message: error.message });
         }
     },
 
-    // Delete a submission
     deleteSubmission: async (req, res) => {
         try {
-            const submission = await Submission.findOneAndDelete({ _id: req.params.id, user: req.user._id });
+            const submission = await Submission.findOneAndDelete(
+                { _id: req.params.id, user: req.user._id }
+            );
             if (!submission) {
                 return res.status(404).json({ message: 'Submission not found or user not authorized' });
             }
@@ -66,7 +105,7 @@ const SubmissionController = {
             res.status(500).json({ message: error.message });
         }
     },
-
 };
 
 module.exports = SubmissionController;
+
--- server/models/submission.model.js
+++ server/models/submission.model.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+const urlValidator = function(v) {
+    return /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/.test(v);
+};
+
 const submissionSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -23,47 +27,33 @@ const submissionSchema = new mongoose.Schema({
     },
     photos: [{
         type: String,
-        validate: {
-            validator: function(v) {
-                return /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/.test(v);
-            },
-            message: props => `${props.value} is not a valid URL!`
-        },
+        validate: [urlValidator, 'Invalid URL']
     }],
     isLGBTQIAOwned: {
         type: Boolean,
-        required: true, // Making this field required to ensure clarity
-        default: false, // Default to false if not provided
+        required: true,
+        default: false,
     },
     isLGBTQIAFriendly: {
         type: Boolean,
-        required: true, // Making this field required to ensure clarity
-        default: false, // Default to false if not provided
+        required: true,
+        default: false,
     },
-    ratings: [{
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        },
-        rating: {
-            type: Number,
-            min: 1,
-            max: 7,
-        },
-    }],
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true, // Making user reference required to ensure every submission is associated with a user
+        required: true,
+    },
+    location: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Location',
+        required: true,
     },
 }, {
     timestamps: true,
 });
 
-// Indexing for better search performance
 submissionSchema.index({ name: 1, categories: 1 });
 submissionSchema.index({ isLGBTQIAOwned: 1, isLGBTQIAFriendly: 1 });
 
 module.exports = mongoose.model('Submission', submissionSchema);
-
-
